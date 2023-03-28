5845d1adf0e1b4866f526446255c9c63671f937b
christiandavidpreci <chikopreci09@gmail.com>
2023-03-28 08:29:46 -0500

    uv3

---
+++ operaciones.py
@@ -0,0 +1,16 @@
+#datos
+#suma
+dato1=int(input("ingrese dato1"))
+dato2=int(input("ingrese dato2"))
+#suma
+suma=dato1+dato2
+print("suma es",suma)
+#resta
+resta=dato1-dato2
+print("resta es",resta)
+#multipliacion
+multiplicacion=dato1*dato2
+print("multiplicacion es",multiplicacion)
+#division
+division=dato1/dato2
+print("division es",division )
