690698018fd26377c0fcbd06967940660e2306ad
christiandavidpreci <chikopreci09@gmail.com>
2023-03-29 10:59:38 -0500

    v4

---
+++ ejerci2.py
@@ -0,0 +1,38 @@
+numero_uno=5
+print(type(numero_uno))
+nombre="david"
+print(type(nombre))
+peso=25.3
+print(type(peso))
+bandera=True
+print(type(bandera))
+
+a=5
+b=5
+print("OPERADORES ARITMETICOS")
+print("la suma es :",a+b)
+print("la resta es:",a-b)
+print("la multiplicacion es :", a*b)
+print("la division es:", a/b)
+print("la division entera es:", a//b)
+print("el modulo es:",a%b)
+print("el exponente:",a**2)
+
+
+print("OPERADORES COMPARACION")
+print("son iguales?",a==b)
+print("son diferentes?",a!=b)
+print("es mayor?",a>b)
+print("es menor?",a<b)
+print("es mayor o igual?",a>=b)
+print("es menor o igual? ",a<=b)
+
+a=5
+b=5
+c=5
+d=10
+print("OPERADORES LOGICOS")
+print("OPERADOR AND", a==b and a==c and b==d)
+print("OPERADOR OR", a==b or a==c or b==d)
+print("OPERADOR NOT",not(a==b))
+
