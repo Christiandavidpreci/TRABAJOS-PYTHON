f526b5d68e02fd95130a5e4ef8032c1b04bac437
christiandavidpreci <chikopreci09@gmail.com>
2023-03-29 12:30:13 -0500

    uv7

---
+++ ejercicios2.py
@@ -0,0 +1,14 @@
+#ejercicios
+#Christian Preciado
+print("ejercicio2")
+print("CURSO DE FUNDAMENTOS DE PYTHON")
+print("NOMBRE: Christian Preciado\n")
+nombre = input("Introduce tu nombre: ")
+print("OPCION1")
+print("¡Hola " + nombre + "!")
+print("OPCION 2")
+print(f"¡Hola :{nombre}")
+print("OPCION 3")
+print("¡Hola : {}".format (nombre))
+print("OPCION 4")
+print("¡Hola:", nombre)
