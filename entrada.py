f4af021c620630fb1d22a80350732180b361e0be
christiandavidpreci <chikopreci09@gmail.com>
2023-03-29 11:36:49 -0500

    uv5

---
+++ entrada.py
@@ -0,0 +1,13 @@
+a=int(input("ingrese numero1"))
+b=int(input("ingrese numero2"))
+suma=a+b
+print("OPCION1")
+print("la suma de :"+ str (a)+" y "+ str(b)+" es: "+str (suma))
+print("OPCION 2")
+print(f"la suma de : {a} y {b} es: {suma}")
+print("OPCION 3")
+print("la suma de : {} y {} es: {}".format (a,b,suma))
+print("OPCION 4")
+print("la suma de:",a,"y",b,"es :", suma)
+
+
