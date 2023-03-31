d90d1a85fc6877c5084d6b1d54f7e350fd4aa664
christiandavidpreci <chikopreci09@gmail.com>
2023-03-31 12:52:50 -0500

    fgdg.v12

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+semaforo=input("cual es el color del semaforo?")
+if semaforo=="verde":
+   print("usted puede cruzar")
+if semaforo=="amarillo":
+   print("detengase")
+if semaforo=="rojo":
+      print("no puede cruzar")
+
+else:
+    print("el estado ingresado es incorrecto")      
+
+
+
+
+     
+
---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+print("curso de fundamentos de python")
+print("christian preciado")
+print("30/30/2023\n")
+
+codigo="daniela"
+contraseña=input("ingrese la contraseña")
+if codigo==contraseña:
+    print("contraseña coincide")
+else:
+    print("contraseña no coincide")    
